4eda1afe29bfeeaa0634e1e75055a6a6de55002f
Melina <melina.gutierrez@ucb.edu.bo>
2022-08-29 23:01:32 -0400

    Creando Pocket Guard

---
+++ src/presenter.js
@@ -0,0 +1,25 @@
+import sumar from "./sumador";
+import multiplicar from "./multiplicador";
+
+const first = document.querySelector("#primer-numero");
+const second = document.querySelector("#segundo-numero");
+const form = document.querySelector("#sumar-form");
+const form2 = document.querySelector("#multiplicar-form");
+const div = document.querySelector("#resultado-div");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+});
